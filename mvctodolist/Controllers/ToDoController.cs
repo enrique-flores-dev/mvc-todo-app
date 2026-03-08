8353a0a274eead889410b32be94d919ba13b86a6
Enrique <enriquemflores.dev@gmail.com>
2026-03-07 22:48:02 -0500

    first commit

---
+++ mvctodolist/Controllers/ToDoController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvctodolist.Models;
+using System.Linq;
+
+namespace mvctodolist.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private static List<ToDoItem> _tasks = new();
+
+        public IActionResult Index()
+        {
+            return View(_tasks);
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(string title)
+        {
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                var item = new ToDoItem
+                {
+                    Id = _tasks.Count + 1,
+                    Title = title.Trim(),
+                    IsComplete = false
+                };
+                _tasks.Add(item);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult CompleteTask(int id)
+        {
+            var task = _tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task != null)
+            {
+                task.IsComplete = true;
+            }
+            return RedirectToAction("Index");
+        }
+        
+        [HttpPost]
+        public IActionResult DeleteTask(int id)
+        {
+            var task = _tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task != null)
+            {
+                _tasks.Remove(task);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ mvctodolist/Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvctodolist.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(120)]
+        public string Title { get; set; } = "";
+
+        public bool IsComplete { get; set; }  
+    }
+}
+
